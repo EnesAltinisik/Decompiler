dc10a3c5dd1a6858f8c083406be249fd5f7f164b
EnesAltinisik <es.altinisik@gmail.com>
2017-03-24 09:36:48 +0300

    bitmis hali

--- resource/forAllProje/sieve/sieve.apk/com/mwr/example/sieve/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.mwr.example.sieve;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = true;
-}
--- resource/forAllProje/sieve/sieve.apk/com/mwr/example/sieve/Manifest.java
+++
@@ -1,9 +0,0 @@
-package com.mwr.example.sieve;
-
-public final class Manifest {
-
-    public static final class permission {
-        public static final String READ_KEYS = "com.mwr.example.sieve.READ_KEYS";
-        public static final String WRITE_KEYS = "com.mwr.example.sieve.WRITE_KEYS";
-    }
-}
